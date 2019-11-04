7b432064ff3e9a300be154f3b855a9138d65608f
Andrew1159 <andrew1159@wp.pl>
2019-11-04 22:06:37 +0100

    Hibernate Database

---
+++ src/main/java/pl/coderstrust/database/HibernateDatabase.java
@@ -0,0 +1,55 @@
+package pl.coderstrust.database;
+
+import java.util.Collection;
+import java.util.Optional;
+import javax.persistence.Entity;
+import pl.coderstrust.model.Invoice;
+@Entity
+public class HibernateDatabase  implements Database {
+
+    private Invoice invoice;
+
+    public HibernateDatabase(Invoice invoice) {
+        this.invoice = invoice;
+    }
+
+    @Override
+    public Invoice save(Invoice invoice) throws DatabaseOperationException {
+        return null;
+    }
+
+    @Override
+    public Optional<Invoice> getById(Long id) throws DatabaseOperationException {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Invoice> getByNumber(String number) throws DatabaseOperationException {
+        return Optional.empty();
+    }
+
+    @Override
+    public Collection<Invoice> getAll() throws DatabaseOperationException {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) throws DatabaseOperationException {
+
+    }
+
+    @Override
+    public void deleteAll() throws DatabaseOperationException {
+
+    }
+
+    @Override
+    public boolean exists(Long id) throws DatabaseOperationException {
+        return false;
+    }
+
+    @Override
+    public long count() throws DatabaseOperationException {
+        return 0;
+    }
+}
