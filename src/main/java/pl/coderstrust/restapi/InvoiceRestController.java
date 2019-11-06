c6644216eb66192d39f43c8fa29940ec3dd9bb95
Ula <ulenca2905@gmail.com>
2019-11-06 22:20:54 +0100

    add method

--- src/main/java/pl/coderstrust/restapi/InvoiceRestController.java
+++ src/main/java/pl/coderstrust/restapi/InvoiceRestController.java
@@ -1,24 +1,53 @@
 package pl.coderstrust.restapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import pl.coderstrust.database.Database;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.coderstrust.database.ServiceOperationException;
+import pl.coderstrust.model.Invoice;
 import pl.coderstrust.services.InvoiceService;
 
+@RestController
+@RequestMapping("/invoices")
 public class InvoiceRestController {
 
     private InvoiceService invoiceService;
-
-    @Autowired
     public InvoiceRestController(InvoiceService invoiceService) {
         this.invoiceService = invoiceService;
     }
 
-    @GetMapping("/invoices")
-    public ResponseEntity getAll() {
-
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> add(@RequestBody(required = false) Invoice invoice) {
+        if (invoice == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        if(invoice.getId() != null) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        try {
+            invoiceService.addInvoice(invoice);
+            return ResponseEntity.ok().build();
+        } catch (ServiceOperationException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
-
+    @GetMapping("/invoices")
+    public Collection<Invoice> getAll() {
+        try {
+            return invoiceService.getAllInvoices();
+        } catch (ServiceOperationException e) {
+            e.printStackTrace();
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return new ArrayList<>();
+        }
+    }
 }
