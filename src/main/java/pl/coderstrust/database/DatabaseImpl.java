bf9fdef3261986603e5f11684ea165ab6fddc70d
Ula <ulenca2905@gmail.com>
2019-11-07 21:17:55 +0100

    first mock mvc test

---
+++ src/main/java/pl/coderstrust/database/DatabaseImpl.java
@@ -0,0 +1,49 @@
+package pl.coderstrust.database;
+
+import java.util.Collection;
+import java.util.Optional;
+import org.springframework.stereotype.Component;
+import pl.coderstrust.model.Invoice;
+
+@Component
+public class DatabaseImpl implements Database {
+    @Override
+    public Invoice save(Invoice invoice) throws DatabaseOperationException {
+        return null;
+    }
+
+    @Override
+    public Optional<Invoice> getById(Long id) throws DatabaseOperationException {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Invoice> getByNumber(String number) throws DatabaseOperationException {
+        return Optional.empty();
+    }
+
+    @Override
+    public Collection<Invoice> getAll() throws DatabaseOperationException {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) throws DatabaseOperationException {
+
+    }
+
+    @Override
+    public void deleteAll() throws DatabaseOperationException {
+
+    }
+
+    @Override
+    public boolean exists(Long id) throws DatabaseOperationException {
+        return false;
+    }
+
+    @Override
+    public long count() throws DatabaseOperationException {
+        return 0;
+    }
+}
