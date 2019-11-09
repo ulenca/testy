30f07a1dedbb01c41eee3c083290d4af811b5e7d
Andrew1159 <andrew1159@wp.pl>
2019-11-09 10:11:16 +0100

    Hibernate Database

--- src/main/java/pl/coderstrust/database/HibernateDatabase.java
+++ src/main/java/pl/coderstrust/database/HibernateDatabase.java
@@ -2,47 +2,109 @@ package pl.coderstrust.database;
 
 import java.util.Collection;
 import java.util.Optional;
+import org.springframework.data.domain.Example;
 import pl.coderstrust.model.Invoice;
 
 public class HibernateDatabase implements Database {
 
+    private final InvoiceRepository repository;
+
+    public HibernateDatabase(InvoiceRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public Invoice save(Invoice invoice) throws DatabaseOperationException {
-        return null;
+        if (invoice == null) {
+            throw new IllegalArgumentException("Invoice cannot be null");
+        }
+        try {
+            return repository.save(invoice);
+        } catch (Exception e) {
+            throw new DatabaseOperationException();
+        }
     }
 
     @Override
     public Optional<Invoice> getById(Long id) throws DatabaseOperationException {
-        return Optional.empty();
+        if (id == null) {
+            throw new IllegalArgumentException("Id cannot be null");
+        }
+        try {
+            return repository.findById(id);
+        } catch (Exception e) {
+            throw new DatabaseOperationException();
+        }
     }
 
     @Override
     public Optional<Invoice> getByNumber(String number) throws DatabaseOperationException {
-        return Optional.empty();
+        if (number == null) {
+            throw new IllegalArgumentException("Number cannot be null");
+        }
+        try {
+            Example<Invoice> invoiceExample = Example.of(Invoice.builder()
+                    .withNumber(number)
+                    .build());
+            return repository.findOne(invoiceExample);
+        } catch (Exception e) {
+            throw new DatabaseOperationException();
+        }
     }
 
     @Override
     public Collection<Invoice> getAll() throws DatabaseOperationException {
+        try {
+            repository.findAll();
+        } catch (Exception e) {
+            throw new DatabaseOperationException();
+        }
         return null;
     }
 
     @Override
     public void delete(Long id) throws DatabaseOperationException {
-
+        if (id < 0) {
+            throw new IllegalArgumentException("Id cannot be lower then one");
+        }
+        try {
+            if (repository.existsById(id)) {
+                repository.deleteById(id);
+            } else {
+                throw new DatabaseOperationException("Invoice does not exist");
+            }
+        } catch (Exception e) {
+            throw new DatabaseOperationException();
+        }
     }
 
     @Override
     public void deleteAll() throws DatabaseOperationException {
-
+        try {
+            repository.deleteAll();
+        } catch (Exception e) {
+            throw new DatabaseOperationException();
+        }
     }
 
     @Override
     public boolean exists(Long id) throws DatabaseOperationException {
-        return false;
+        if (id == null) {
+            throw new IllegalArgumentException("Id cannot be lower then zero");
+        }
+        try {
+            return repository.existsById(id);
+        } catch (Exception e) {
+            throw new DatabaseOperationException();
+        }
     }
 
     @Override
     public long count() throws DatabaseOperationException {
-        return 0;
+        try {
+            return repository.count();
+        } catch (Exception e) {
+            throw new DatabaseOperationException();
+        }
     }
 }
