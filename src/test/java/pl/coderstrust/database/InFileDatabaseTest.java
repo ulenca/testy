05fe4e0268bf5d1c3eab4724d1f29207fc801d48
tomj23 <tomasz.stolarczyk@gmail.com>
2019-11-21 23:09:51 +0100

    2 tests

--- src/test/java/pl/coderstrust/database/InFileDatabaseTest.java
+++ src/test/java/pl/coderstrust/database/InFileDatabaseTest.java
@@ -1,15 +1,20 @@
 package pl.coderstrust.database;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.concurrent.atomic.AtomicLong;
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.coderstrust.configuration.ApplicationConfiguration;
 import pl.coderstrust.configuration.InFileDatabaseProperties;
+import pl.coderstrust.generators.InvoiceGenerator;
 import pl.coderstrust.helpers.FileHelper;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,14 +26,30 @@ class InFileDatabaseTest {
     @Mock
     private InFileDatabaseProperties properties;
 
-    @InjectMocks
-    private InFileDatabase inFileDatabase;
+    private ObjectMapper objectMapper;
 
-    private ObjectMapper mapper;
-    private AtomicLong nextId;
+    private InFileDatabase database;
+
+    @BeforeEach
+    void setup() throws IOException {
+        objectMapper = new ApplicationConfiguration().getObjectMapper();
+        database = new InFileDatabase(fileHelper, objectMapper, properties);
+    }
 
     @Test
     void saveMethodShouldThrowExceptionForNullAsInvoice() {
-        assertThrows(IllegalArgumentException.class, () -> inFileDatabase.save(null));
+        assertThrows(IllegalArgumentException.class, () -> database.save(null));
+    }
+
+    @Test
+    void shouldReturnNumberOfInvoices() throws IOException, DatabaseOperationException {
+        List<String> invoices = List.of(objectMapper.writeValueAsString(InvoiceGenerator.generateRandomInvoice())
+            , objectMapper.writeValueAsString(InvoiceGenerator.generateRandomInvoice())
+            , objectMapper.writeValueAsString(InvoiceGenerator.generateRandomInvoice()));
+        when(fileHelper.readLines(properties.getFilePath())).thenReturn(invoices);
+
+        long result = database.count();
+
+        assertEquals(invoices.size(), result);
     }
 }
