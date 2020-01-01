d9e9d195a79c8625f82aa7ca5254eda164109f58
Andrew1159 <andrew1159@wp.pl>
2020-01-01 14:47:36 +0100

    Add Basic Auth

--- src/main/java/pl/coderstrust/database/DatabaseOperationException.java
+++ src/main/java/pl/coderstrust/database/DatabaseOperationException.java
@@ -1,19 +1,19 @@
 package pl.coderstrust.database;
 
 public class DatabaseOperationException extends Exception {
-    public DatabaseOperationException() {
+public DatabaseOperationException() {
         super();
-    }
+        }
 
-    public DatabaseOperationException(String message) {
+public DatabaseOperationException(String message) {
         super(message);
-    }
+        }
 
-    public DatabaseOperationException(Throwable cause) {
+public DatabaseOperationException(Throwable cause) {
         super(cause);
-    }
+        }
 
-    public DatabaseOperationException(String message, Throwable cause) {
+public DatabaseOperationException(String message, Throwable cause) {
         super(message, cause);
-    }
-}
+        }
+        }
