b67485750d6636a039f0b699e9b654f8eb52dac5
Andrew1159 <andrew1159@wp.pl>
2020-01-06 22:12:34 +0100

    Add pdf to email

--- src/main/java/pl/coderstrust/services/InvoiceEmailServiceImpl.java
+++ src/main/java/pl/coderstrust/services/InvoiceEmailServiceImpl.java
@@ -1,10 +1,11 @@
 package pl.coderstrust.services;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -15,11 +16,13 @@ public class InvoiceEmailServiceImpl implements InvoiceEmailService {
 
     private JavaMailSender emailSender;
     private MailProperties mailProperties;
+    private InvoicePdfService invoicePdfService;
 
     @Autowired
-    public InvoiceEmailServiceImpl(JavaMailSender emailSender, MailProperties mailProperties) {
+    public InvoiceEmailServiceImpl(JavaMailSender emailSender, MailProperties mailProperties, InvoicePdfService invoicePdfService) {
         this.emailSender = emailSender;
         this.mailProperties = mailProperties;
+        this.invoicePdfService = invoicePdfService;
     }
 
     @Async
@@ -27,13 +30,17 @@ public class InvoiceEmailServiceImpl implements InvoiceEmailService {
         if (invoice == null) {
             throw new IllegalArgumentException("Invoice cannot be null!");
         }
-        MimeMessage message = new MimeMessage();
-        MimeMessageHelper messageHelper = new MimeMessageHelper();
-        // Poczytać dokumentacje od ww jak dodać załącznik do maila
-        message.setTo(mailProperties.getProperties().get("to"));
-        message.setFrom(mailProperties.getUsername());
-        message.setSubject(mailProperties.getProperties().get("subject"));
-        message.setText(mailProperties.getProperties().get("content"));
-        emailSender.send(message);
+        try {
+            MimeMessage message = emailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(mailProperties.getProperties().get("to"));
+            helper.setFrom(mailProperties.getUsername());
+            helper.setSubject(mailProperties.getProperties().get("subject"));
+            helper.setText(mailProperties.getProperties().get("content"));
+            helper.addAttachment(String.format("%s.pdf", invoice.getNumber()), new ByteArrayResource(invoicePdfService.createPdf(invoice)));
+            emailSender.send(message);
+        } catch (MessagingException | ServiceOperationException e) {
+            e.printStackTrace();
+        }
     }
 }
